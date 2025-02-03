94182f8b45c4d2b7ba6b88f1177a58025f97f198
stskri <ymt.skri@gmail.com>
2025-02-03 14:17:55 +0000

    Add columns to Game-model and WordEntry-model

--- db/migrate/20250203140821_create_games.rb
+++ db/migrate/20250203140821_create_games.rb
@@ -2,6 +2,8 @@ class CreateGames < ActiveRecord::Migration[6.1]
   def change
     create_table :games do |t|
 
+      t.integer :status, null: false, default: "0"
+
       t.timestamps
     end
   end
--- db/migrate/20250203140931_create_word_entries.rb
+++ db/migrate/20250203140931_create_word_entries.rb
@@ -2,6 +2,9 @@ class CreateWordEntries < ActiveRecord::Migration[6.1]
   def change
     create_table :word_entries do |t|
 
+      t.integer :game_id, null: false
+      t.integer :word_entry, null: false
+
       t.timestamps
     end
   end
