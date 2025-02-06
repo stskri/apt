6be36c6bb30c53be2773d1b793afad2e421a6250
stskri <ymt.skri@gmail.com>
2025-02-06 13:19:32 +0000

    Add game.word_entries.create to games#create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,9 @@ class GamesController < ApplicationController
   def create
     game = Game.new
     if game.save
-      
+      game.word_entries.create(
+        word_entry: WordEntry.word_entries.values.sample
+      )
       redirect_to games_path
     end
   end
