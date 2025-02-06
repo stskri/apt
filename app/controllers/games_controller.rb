6cd4a53a1dbfd1dad4a93e25f383820a5317c2af
stskri <ymt.skri@gmail.com>
2025-02-06 12:56:26 +0000

    Add games#create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,10 @@ class GamesController < ApplicationController
   def index
     @game = Game.new
   end
+
+  def create
+    game = Game.new
+    game.save
+    redirect_to games_path
+  end
 end
--- app/models/word_entry.rb
+++ app/models/word_entry.rb
@@ -1,2 +1,3 @@
 class WordEntry < ApplicationRecord
+  enum word_entry: {a_pt: 0, apt: 1, uh: 2, ahah: 3}
 end
